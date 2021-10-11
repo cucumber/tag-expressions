10a4c80074994210ff75e45a0461d2b53fb7f542
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2021-10-11 10:04:46 +0100

    Try to build with perl

--- perl/default.mk
+++ perl/default.mk
@@ -5,17 +5,6 @@
 SHELL := /usr/bin/env bash
 ALPINE := $(shell which apk 2> /dev/null)
 
-### COMMON stuff for all platforms
-
-BERP_VERSION = 1.3.0
-BERP_GRAMMAR = gherkin.berp
-
-define berp-generate-parser =
--! dotnet tool list --tool-path /usr/bin | grep "berp\s*$(BERP_VERSION)" && dotnet tool update Berp --version $(BERP_VERSION) --tool-path /usr/bin
-berp -g $(BERP_GRAMMAR) -t $< -o $@ --noBOM
-endef
-
-
 ### Common targets for all functionalities implemented on Perl
 
 default: test
@@ -43,7 +32,7 @@ endif
 .PHONY: update-version
 
 .cpanfile_dependencies: cpanfile
-	PERL5LIB=${PERL5LIB} cpanm --notest --local-lib ./perl5 --installdeps .
+	cpanm --notest --installdeps .
 	touch $@
 
 predistribution: dist-clean test CHANGELOG.md
