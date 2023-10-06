b93923f186b9f53270bac7b12ccb94e5389d96e6
Luke Hill <lukehill_uk@hotmail.com>
2023-10-06 16:26:02 +0100

    Remove all redundant items and use let's in specs

--- ruby/spec/errors_spec.rb
+++ ruby/spec/errors_spec.rb
@@ -5,8 +5,9 @@ tests = YAML.load_file('../testdata/errors.yml')
 
 describe 'Errors' do
   tests.each do |test|
+    let(:parser) { Cucumber::TagExpressions::Parser.new }
+
     it %Q{fails to parse "#{test['expression']}" with "#{test['error']}"} do
-      parser = Cucumber::TagExpressions::Parser.new
       expect { parser.parse(test['expression']) }.to raise_error(test['error'])
     end
   end
--- ruby/spec/evaluations_spec.rb
+++ ruby/spec/evaluations_spec.rb
@@ -6,9 +6,10 @@ evaluations = YAML.load_file('../testdata/evaluations.yml')
 describe 'Evaluations' do
   evaluations.each do |evaluation|
     context evaluation['expression'] do
+      let(:parser) { Cucumber::TagExpressions::Parser.new }
+
       evaluation['tests'].each do |test|
         it "evaluates [#{test['variables'].join(', ')}] to #{test['result']}" do
-          parser = Cucumber::TagExpressions::Parser.new
           expect(parser.parse(evaluation['expression']).evaluate(test['variables'])).to eq(test['result'])
         end
       end
